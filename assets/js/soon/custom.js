6a72d3cd6503ddf2bfa283ce4656405cc9933c5a
Tito Lins <tito@blinx.com.br>
2015-07-06 20:03:41 -0300

    finished site

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -6,7 +6,7 @@ COMMING SOON PAGE
     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
     * according to the GMT+0 Timezone
     **/
-    var launch = new Date(2015, 08, 01, 12, 00);
+    var launch = new Date(2015, 06, 31, 12, 00);
     /**
     * The script
     **/
